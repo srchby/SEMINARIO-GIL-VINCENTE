854ea27b5d0173a594687459362e7a5c25cbb167
srchby <139605844+srchby@users.noreply.github.com>
2023-11-08 16:07:49 +0000

    changes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+const pointsContainer = document.getElementById('points')
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -82,48 +83,48 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'Qual a fonte de energia mais abundante na Terra?',
+    question: 'Qual das alternativas abaixo NÃO faz parte das obras de Gil Vicente?',
     answers: [
-      { text: 'Energia Solar', correct: true },
-      { text: 'Energia Nuclear', correct: false },
-      { text: 'Energia Eólica', correct: false },
-      { text: 'Petróleo', correct: false }
+      { text: 'O auto do bom sucesso', correct: true },
+      { text: 'O auto da barca do inferno', correct: false },
+      { text: 'A farça de Inés Pereira', correct: false },
+      { text: 'O velho da horta', correct: false }
     ]
   },
   {
-    question: 'O que significa a sigla "ODS"?',
+    question: 'Qual foi a primeira obra publicada de Gil Vicente?',
     answers: [
-      { text: 'Ondas de Desenvolvimento Sustentável', correct: false },
-      { text: 'Organização de Desenvolvimento Solar', correct: false },
-      { text: 'Otimização de Desenvolvimento Sustentável', correct: false },
-      { text: 'Objetivos de Desenvolvimento Sustentável', correct: true }
-    ]
-  },
-    {
-    question: 'Qual o tipo de energia que é gerado pelo movimento do ar?',
-    answers: [
-      { text: 'Energia Solar', correct: false },
-      { text: 'Energia Eólica', correct: true },
-      { text: 'Biomassa', correct: false },
-      { text: 'Energia Hidrelétrica', correct: false }
+      { text: 'Auto da Barca do Inferno', correct: false },
+      { text: 'Quem tem Farelos?', correct: false },
+      { text: 'Auto da Alma', correct: false },
+      { text: 'Monologo do Vaqueiro', correct: true }
     ]
   },
   {
-    question: 'Qual é a fonte de energia mais usada globalmente para produção de eletricidade?',
+    question: 'Gil Vicente escreveu o Auto da Barca do Inferno em 1517, no momento em que eclodia na Alemanha a Reforma Protestante com a crítica veemente de Lutero ao mau clero dominante na Igreja. Nessa obra, há a figura do frade, severamente censurado como um sacerdote negligente. Indique a alternativa cujo conteúdo não se presta a caracterizar, na referida peça, os erros cometidos pelo religioso.',
     answers: [
-      { text: 'Energia Eólica', correct: false },
-      { text: 'Energia Solar', correct: false },
-      { text: 'Carvão', correct: true },
-      { text: 'Energia Nuclear', correct: false }
+      { text: 'Não cumprir os votos de celibato, mantendo a concubina Florença.', correct: false },
+      { text: 'Praticar a avareza como cúmplice do Fidalgo, e a exploração da prostituição em parceria com a alcoviteira.', correct: true },
+      { text: 'Entregar-se a práticas mundanas, como a dança.', correct: false },
+      { text: 'Praticar esgrima e usar armamentos de guerra, proibidos aos clérigos.', correct: false }
     ]
   },
   {
-    question: 'Qual país mais produz energia eólica?',
+    question: 'Sobre o Auto da barca do Inferno, de Gil Vicente, é incorreto afirmar:',
     answers: [
-      { text: 'Noruega', correct: false },
-      { text: 'Índia', correct: false },
-      { text: 'China', correct: true },
-      { text: 'Alemanha', correct: false }
+      { text: 'O autor apresenta severa crítica à prepotência e tirania dos nobres, e à desonestidade e corrupção dos homens da lei.', correct: false },
+      { text: 'O diabo atua como agente crítico, que revela as mentiras e falsidades das personagens.', correct: false },
+      { text: 'Na luta entre o Bem e o Mal são favorecidos aqueles que em vida pertenceram à classe social privilegiada.', correct: true },
+      { text: 'O autor vale-se do tema do Juízo Final para estabelecer uma crítica à sociedade, fazendo desfilar em cena os tipos sociais identificados através de suas qualidades e defeitos.', correct: false }
+    ]
+  },
+  {
+    question: 'Qual área do renascimento Gil Vicente se encontra?',
+    answers: [
+      { text: 'Capitalismo', correct: false },
+      { text: 'Cubismo', correct: false },
+      { text: 'Humanismo', correct: true },
+      { text: 'Feudalismo', correct: false }
     ]
   }
 ]
